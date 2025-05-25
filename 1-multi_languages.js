0bfc336340ffe3688d1e1c71498479c37f7c52cc
Machesa <125511859+Machesa@users.noreply.github.com>
2025-05-25 13:47:16 +0200

    Update 1-multi_languages.js

--- 1-multi_languages.js
+++ 1-multi_languages.js
@@ -1,6 +1,6 @@
 let C = "C in fun"
 console.log(C);
-let myVar = "Python is cool"
-console.log(myVar);
+let myvar = "Python is cool"
+console.log(myvar);
 let Java = "JavaScript is amazing"
 console.log(Java);
